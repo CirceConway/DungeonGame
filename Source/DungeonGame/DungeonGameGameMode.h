de23ed3ecf814e7cd29421ce5670220f57706ca4
Circe <circeconway@gmail.com>
2024-01-26 14:06:02 -0700

    First commit, third person template

---
+++ Source/DungeonGame/DungeonGameGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DungeonGameGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ADungeonGameGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ADungeonGameGameMode();
+};
+
+
+
