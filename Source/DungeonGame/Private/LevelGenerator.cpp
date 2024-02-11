73bc9cea63189fb37cb9f2f0db841a05d4cdb2b0
Circe <circeconway@gmail.com>
2024-02-10 17:23:28 -0700

    Simple 2D voronoi generator working

--- Source/DungeonGame/Private/LevelGenerator.cpp
+++ Source/DungeonGame/Private/LevelGenerator.cpp
@@ -1,69 +1,117 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "LevelGenerator.h"
 
-const int xSize = 100;
-const int ySize = 100;
-//const int z_size = 100;
+const static int xSize = 50;
+const static int ySize = 50;
+const static int zSize = 50;
 
-struct voronoiGraph {
-	int graph[xSize][ySize];
-};
+const static int numPoints = 6;
 
 // Sets default values
 ALevelGenerator::ALevelGenerator()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
 }
 
-// Called when the game starts or when spawned
-void ALevelGenerator::BeginPlay()
+int ALevelGenerator::CalcManhattanDistance(struct Point p1, struct Point p2)
 {
-	Super::BeginPlay();
-	
+	int distX = abs(p1.x - p2.x);
+	int distY = abs(p1.y - p2.y);
+
+	return distX + distY;
 }
 
 //Creates paths of the floor, returns int array of [size * size]
 //Each index represents space for one potential room
-voronoiGraph CreateVoronoi(int points)
+ALevelGenerator::VoronoiGraph ALevelGenerator::CreateVoronoi()
 {
-	voronoiGraph voronoi;
+	VoronoiGraph voronoi;
 
-	for (int i = 0; i < points; i++)
+	//Place the seeds on the voronoi graph
+	for (int i = 0; i < numPoints; i++)
 	{
+		
 		bool success = false;
 
 		while (!success)
 		{
-			int randX = rand() % xSize;
-			int randY = rand() % ySize;
-			if (voronoi.graph[randX][randY] == 0)
+			struct Point point(rand() % xSize, rand() % ySize);
+
+			if (voronoi.graph[point.x][point.y] != 9)
 			{
-				voronoi.graph[randX][randY] = 1;
+				voronoi.SetPoint(point, 9);
+				voronoi.pointList[i] = point;
+
 				success = true;
 			}
-		}
+		}		
+	}
 
-		
+	for (int i = 0; i < xSize; i++)
+	{
+		for (int k = 0; k < ySize; k++)
+		{
+			int shortestDist = xSize + ySize + zSize + 1;
+			int shortestIndex = 0;
+			struct Point p1(i, k);
+
+			for (int j = 0; j < numPoints; j++)
+			{
+				int dist = CalcManhattanDistance(p1, voronoi.pointList[j]);
+
+				if (dist < shortestDist)
+				{
+					shortestDist = dist;
+					shortestIndex = j;
+				}
+			}
+			if (voronoi.graph[p1.x][p1.y] != 9)
+			{
+				voronoi.SetPoint(p1, shortestIndex);
+			}
+			
+		}
 	}
 
 	return voronoi;
 }
 
 //Use given graph to spawn rooms in the world
-void SpawnTilesFromGraph(int &graph)
+void ALevelGenerator::SpawnTilesFromGraph(VoronoiGraph graph)
 {
-	;
+	for (int i = 0; i < xSize; i++)
+	{
+		std::string printString = "";
+		for (int k = 0; k < ySize; k++)
+		{
+			printString.append(std::to_string(graph.graph[i][k]));
+			
+		}
+		UE_LOG(LogTemp, Warning, TEXT("%s"), *FString(printString.c_str()));
+	}
 }
 
-// Called every frame
+// Called when the game starts or when spawned
+void ALevelGenerator::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UE_LOG(LogTemp, Warning, TEXT("STARTING THE CREATION PROCESS"));
+	VoronoiGraph myVoronoi = CreateVoronoi();
+	UE_LOG(LogTemp, Warning, TEXT("FINISHED CREATION"));
+	SpawnTilesFromGraph(myVoronoi);
+
+}
+
+/* Called every frame
 void ALevelGenerator::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
 }
+*/
 
 
--- Source/DungeonGame/Private/LevelGenerator.h
+++ Source/DungeonGame/Private/LevelGenerator.h
@@ -6,15 +6,53 @@
 #include "GameFramework/Actor.h"
 #include "LevelGenerator.generated.h"
 
+
 UCLASS()
 class ALevelGenerator : public AActor
 {
 	GENERATED_BODY()
-	
+
 public:	
+
+	const static int xSize = 50;
+	const static int ySize = 50;
+	const static int zSize = 50;
+
+	const static int numPoints = 6;
+
+	struct Point {
+		int x, y;
+		//int z;
+
+		Point(int x, int y)
+		{
+			this->x = x;
+			this->y = y;
+		}
+
+		Point()
+		{
+			x = 0;
+			y = 0;
+		}
+
+	};
+
+	class VoronoiGraph {
+	public:
+		int graph[xSize][ySize];
+		struct Point pointList[numPoints];
+
+		void SetPoint(struct Point p, int val)
+		{
+			graph[p.x][p.y] = val;
+		}
+	};
 
 	// Sets default values for this actor's properties
 	ALevelGenerator();
+
+	//
 
 protected:
 	// Called when the game starts or when spawned
@@ -22,13 +60,13 @@ protected:
 
 	//Creates paths of the floor, returns int array of [size * size]
 	//Each index represents space for one potential room
-	voronoiGraph CreateVoronoi(int points);
+	VoronoiGraph CreateVoronoi();
 
 	//Use given graph to spawn tiles in the world
-	void SpawnTilesFromGraph(int &graph);
+	void SpawnTilesFromGraph(VoronoiGraph graph);
 
-public:	
+	int CalcManhattanDistance(struct Point p1, struct Point p2);
+
 	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-	
+	//virtual void Tick(float DeltaTime) override;
 };
