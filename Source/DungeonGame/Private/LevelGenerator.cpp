394de86f29a22a6f92f73c32fbaad558190f89d0
Circe <circeconway@gmail.com>
2024-02-12 19:07:32 -0700

    Memory errors :(

--- Source/DungeonGame/Private/LevelGenerator.cpp
+++ Source/DungeonGame/Private/LevelGenerator.cpp
@@ -2,9 +2,7 @@
 
 #include "LevelGenerator.h"
 
-//const static int xSize = 50;
-//const static int ySize = 50;
-//const static int zSize = 50;
+
 
 //const static int numPoints = 8;
 
@@ -27,6 +25,14 @@ ALevelGenerator::ALevelGenerator()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
+	xSize = 150;
+	ySize = 150;
+	zSize = 150;
+
+	tileSize = 1200;
+	tileHeight = 480;
+	numSeeds = 40;
+
 
 }
 
@@ -39,14 +45,29 @@ int ALevelGenerator::CalcManhattanDistance(struct Point p1, struct Point p2)
 	return distX + distY + distZ;
 }
 
+int ALevelGenerator::CalcPythagoreanDistance(struct Point p1, struct Point p2)
+{
+	int distX = (p1.x - p2.x)^2;
+	int distY = (p1.y - p2.y)^2;
+	int distZ = (p1.z - p2.z)^2;
+
+	return (int)sqrt(distX + distY + distZ);
+}
+
+
+void ALevelGenerator::EndGeneration(VoronoiGraph* graph)
+{
+	delete graph;
+}
+
 //Creates paths of the floor, returns int array of [size * size]
 //Each index represents space for one potential room
 ALevelGenerator::VoronoiGraph ALevelGenerator::CreateVoronoi()
 {
-	VoronoiGraph voronoi;
+	VoronoiGraph voronoi(xSize, ySize, zSize, numSeeds);
 
 	//Place the seeds on the voronoi graph
-	for (int i = 0; i < numPoints; i++)
+	for (int i = 0; i < numSeeds; i++)
 	{
 		
 		bool success = false;
@@ -55,7 +76,7 @@ ALevelGenerator::VoronoiGraph ALevelGenerator::CreateVoronoi()
 		{
 			struct Point point(rand() % xSize, rand() % ySize, rand() % zSize);
 
-			if (voronoi.graph[point.x][point.y][point.z] != -1)
+			if (voronoi.GetPoint(point) != -1)
 			{
 				voronoi.SetPoint(point, -1);
 				voronoi.seedsList[i] = point;
@@ -76,8 +97,9 @@ ALevelGenerator::VoronoiGraph ALevelGenerator::CreateVoronoi()
 				int shortestDist = xSize + ySize + zSize + 1;
 				int shortestIndex = 0;
 				struct Point p1(i, k, z);
-				for (int j = 0; j < numPoints; j++)
+				for (int j = 0; j < numSeeds; j++)
 				{
+					//int dist = CalcPythagoreanDistance(p1, voronoi.seedsList[j]);
 					int dist = CalcManhattanDistance(p1, voronoi.seedsList[j]);
 
 					if (dist < shortestDist)
@@ -86,11 +108,14 @@ ALevelGenerator::VoronoiGraph ALevelGenerator::CreateVoronoi()
 						shortestIndex = j;
 					}
 				}
+				voronoi.SetPoint(p1, shortestIndex + 2);
+				/*
 				if (voronoi.graph[p1.x][p1.y][p1.z] != -1)
 				{
-					voronoi.SetPoint(p1, shortestIndex + 2);
+					
 					//UE_LOG(LogTemp, Warning, TEXT("Point (%d, %d) is %d"), p1.x, p1.y, voronoi.graph[p1.x][p1.y]);
 				}
+				*/
 			}
 				
 		}
@@ -100,7 +125,7 @@ ALevelGenerator::VoronoiGraph ALevelGenerator::CreateVoronoi()
 
 //Scans left to right, marking the locations where numbers change with a 1, and everything else with a 0
 //All -1s become 0s
-void ALevelGenerator::TraceEdges(VoronoiGraph* graph)
+void ALevelGenerator::TraceEdges_BAD(VoronoiGraph* graph)
 {
 	for (int i = 0; i < xSize - 1; i++)
 	{
@@ -109,7 +134,8 @@ void ALevelGenerator::TraceEdges(VoronoiGraph* graph)
 			for (int j = 0; j < zSize - 1; j++)
 			{
 				struct Point p(i, k, j);
-				if (((graph->graph[i][k][j] == graph->graph[i][k + 1][j])
+				/*
+				if (((graph->GetPoint(p) == graph->GetPoint(i + 1k,j))
 					&& (graph->graph[i][k][j] == graph->graph[i + 1][k][j])
 					&& (graph->graph[i][k][j] == graph->graph[i][k][j + 1]))
 					|| (graph->graph[i + 1][k][j] == -1)
@@ -126,6 +152,7 @@ void ALevelGenerator::TraceEdges(VoronoiGraph* graph)
 				{
 					graph->SetPoint(p, 1);
 				}
+				*/
 			}
 			//The end of this sets the last x, y and z coords to 0 instead of actually testing them
 			//TODO: change when not feeling lazy
@@ -162,6 +189,52 @@ void ALevelGenerator::TraceEdges(VoronoiGraph* graph)
 	*/
 }
 
+//QUINTUPLE NESTED FOR LOOP BABBEEEEEEEEEEEEE
+//Dear future Circe, I'm so sorry
+ALevelGenerator::VoronoiGraph ALevelGenerator::ConvolutionalTrace(VoronoiGraph graph)
+{
+	VoronoiGraph edges(xSize, ySize, zSize, numSeeds);
+	int neighbors[6] = { 0, 0, 0, 0, 0, 0 };
+	int regions;
+	//Iterate through the graph
+	for (int i = 1; i < xSize - 1; i++)
+	{
+		for (int k = 1; k < ySize - 1; k++)
+		{
+			for (int j = 1; j < zSize - 1; j++)
+			{
+				regions = 0;
+
+				neighbors[0] = graph.GetPoint(i - 1, k, j);
+				neighbors[1] = graph.GetPoint(i + 1, k, j);
+				neighbors[2] = graph.GetPoint(i, k - 1, j);
+				neighbors[3] = graph.GetPoint(i, k + 1, j);
+				neighbors[4] = graph.GetPoint(i, k, j - 1);
+				neighbors[5] = graph.GetPoint(i, k, j + 1);
+
+				//Count how many unique regions are next to the point
+				for (int x = 1; x < 6; x++)
+				{
+					int y;
+					for (y = 0; y < x; y++)
+					{
+						if (neighbors[x] == neighbors[y])
+							break;
+					}
+					if (x == y)
+						regions++;
+				}
+
+				edges.SetPoint(Point(i, j, k), regions);
+
+
+			}
+		}
+	}
+
+	return edges;
+}
+
 //Use given graph to spawn rooms in the world
 void ALevelGenerator::SpawnTilesFromGraph(VoronoiGraph graph)
 {
@@ -171,7 +244,7 @@ void ALevelGenerator::SpawnTilesFromGraph(VoronoiGraph graph)
 		{
 			for (int j = 0; j < zSize; j++)
 			{
-				if (graph.graph[i][k][j] == 1)
+				if (graph.GetPoint(i, k, j) > 1)
 				{
 					FVector spawnLocation = GetTransform().GetLocation();
 					FVector spawnOffset = FVector(i * tileSize, k * tileSize, j * tileHeight);
@@ -194,10 +267,10 @@ void ALevelGenerator::PrintGraphToConsole(VoronoiGraph graph)
 
 		for (int k = 0; k < ySize; k++)
 		{
-			printString.append(std::to_string(graph.graph[i][k][zSize-1]));
+			//printString.append(std::to_string(graph.graph[i][k][zSize-1]));
 		}
 
-		UE_LOG(LogTemp, Warning, TEXT("%s"), *FString(printString.c_str()));
+		//UE_LOG(LogTemp, Warning, TEXT("%s"), *FString(printString.c_str()));
 	}
 }
 
@@ -213,9 +286,11 @@ void ALevelGenerator::BeginPlay()
 	VoronoiGraph myVoronoi = CreateVoronoi();
 	//UE_LOG(LogTemp, Warning, TEXT("FINISHED CREATION, STARTING TRACE"));
 	//PrintGraphToConsole(myVoronoi);
-	TraceEdges(&myVoronoi);
+	VoronoiGraph edges = ConvolutionalTrace(myVoronoi);
+	//EndGeneration(&myVoronoi);
 	//PrintGraphToConsole(myVoronoi);
-	SpawnTilesFromGraph(myVoronoi);
+	SpawnTilesFromGraph(edges);
+	//EndGeneration(&edges);
 
 }
 
--- Source/DungeonGame/Private/LevelGenerator.h
+++ Source/DungeonGame/Private/LevelGenerator.h
@@ -21,15 +21,20 @@ public:
 	UFUNCTION()
 	void SpawnTestTile();
 
-	
-	const static int xSize = 50;
-	const static int ySize = 50;
-	const static int zSize = 50;
+	UPROPERTY(EditDefaultsOnly, Category = "Generation Settings")
+	int xSize;
+	UPROPERTY(EditDefaultsOnly, Category = "Generation Settings")
+	int ySize;
+	UPROPERTY(EditDefaultsOnly, Category = "Generation Settings")
+	int zSize;
 
-	const static int tileSize = 1200;
-	const static int tileHeight = 480;
+	UPROPERTY(EditDefaultsOnly, Category = "Generation Settings")
+	int tileSize;
+	UPROPERTY(EditDefaultsOnly, Category = "Generation Settings")
+	int tileHeight;
 	
-	const static int numPoints = 15;
+	UPROPERTY(EditDefaultsOnly, Category = "Generation Settings")
+	int numSeeds;
 
 	struct Point {
 		int x, y, z;
@@ -51,29 +56,55 @@ public:
 	};
 
 	class VoronoiGraph {
+	private:
+		int xSize, ySize, zSize;
+		int numSeeds;
+
+		int* graph = new int[xSize * ySize * zSize];
+
 	public:
-		int graph[xSize][ySize][zSize];
-		struct Point seedsList[numPoints];
+		
+		struct Point *seedsList = new struct Point[numSeeds];
 
 		void SetPoint(struct Point p, int val)
 		{
-			graph[p.x][p.y][p.z] = val;
+			graph[p.x 
+				+ p.y * ySize 
+				+ p.z * ySize * zSize] = val;
 		}
 
-		VoronoiGraph()
+		int GetPoint(struct Point p)
 		{
-			for (int i = 0; i < xSize; i++)
+			return graph[p.x
+				+ p.y * ySize
+				+ p.z * ySize * zSize];
+		}
+		int GetPoint(int x, int y, int z)
+		{
+			return graph[x
+				+ y * ySize
+				+ z * ySize * zSize];
+		}
+
+		VoronoiGraph(int xSize, int ySize, int zSize, int numSeeds)
+		{
+			this->xSize = xSize;
+			this->ySize = ySize;
+			this->zSize = zSize;
+			this->numSeeds = numSeeds;
+
+			for (int i = 0; i < xSize * ySize * zSize; i++)
 			{
-				for (int k = 0; k < ySize; k++)
-				{
-					for (int j = 0; j < zSize; j++)
-					{
-						graph[i][k][j] = 0;
-					}
-					
-				}
+				graph[i] = 0;
 			}
 		}
+
+		~VoronoiGraph()
+		{
+			//delete[] graph;
+		}
+
+
 	};
 
 	// Sets default values for this actor's properties
@@ -87,7 +118,9 @@ protected:
 	//Each index represents space for one potential room
 	VoronoiGraph CreateVoronoi();
 
-	void TraceEdges(VoronoiGraph* graph);
+	void TraceEdges_BAD(VoronoiGraph* graph);
+
+	VoronoiGraph ConvolutionalTrace(VoronoiGraph graph);
 
 	//Use given graph to spawn tiles in the world
 	void SpawnTilesFromGraph(VoronoiGraph graph);
@@ -95,6 +128,10 @@ protected:
 	void PrintGraphToConsole(VoronoiGraph graph);
 
 	int CalcManhattanDistance(struct Point p1, struct Point p2);
+
+	int CalcPythagoreanDistance(struct Point p1, struct Point p2);
+
+	void EndGeneration(VoronoiGraph* graph);
 
 	// Called every frame
 	//virtual void Tick(float DeltaTime) override;
